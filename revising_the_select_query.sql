67d77efe1e3b2ce7210e92f9f1c24e93ed3d5a2f
Sunidhi Thakur <38562071+sunidhi1404@users.noreply.github.com>
2022-09-13 12:20:08 +0530

    Update revising_the_select_query.sql

--- revising_the_select_query.sql
+++ revising_the_select_query.sql
@@ -2,7 +2,7 @@
 Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
 The CITY table is described as follows:
 
-FIELD            TYPE
+FIELD           TYPE
 ID              NUMBER
 NAME            VARCHAR(17)
 COUNTRYCODE     VARCHAR(3)

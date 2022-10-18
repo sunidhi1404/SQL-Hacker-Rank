30a40c3d699e373b4dd2eec6cde4d4c16e1d9ac5
Sunidhi Thakur <38562071+sunidhi1404@users.noreply.github.com>
2022-10-18 14:50:55 +0530

    Update weather_observation_station_5.sql

--- weather_observation_station_5.sql
+++ weather_observation_station_5.sql
@@ -1,13 +1,14 @@
 /*
 Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
 If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
-The CITY table is described as follows:
+The STATION table is described as follows:
 FIELD           TYPE
 ID              NUMBER
-NAME            VARCHAR2(17)
-COUNTRYCODE     VARCHAR2(3)
-DISTRICT        VARCHAR2(20)
-POPULATION      NUMBER
+CITY            VARCHAR2(21)
+STATE           VARCHAR2(2)
+LAT_N           NUMBER
+LONG_W          NUMBER
+where LAT_N is the northern latitude and LONG_W is the western longitude.
 For example, CITY has four entries: DEF, ABC, PQRS and WXY. When ordered alphabetically, the CITY names are listed as ABC, DEF, PQRS, and WXY, with lengths 3,3,4,3 and
 the longest name is PQRS, but there are 3 options for shortest named city. Choose ABC, because it comes first alphabetically.
 OUTPUT: ABC 3

cf49cc392d3dd20d38220ccd9f2c98e878f66ca4
Sunidhi Thakur <38562071+sunidhi1404@users.noreply.github.com>
2022-10-18 15:18:02 +0530

    Create weather_observation_station_9.sql

---
+++ weather_observation_station_9.sql
@@ -0,0 +1,14 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+The STATION table is described as follows:
+FIELD           TYPE
+ID              NUMBER
+CITY            VARCHAR2(21)
+STATE           VARCHAR2(2)
+LAT_N           NUMBER
+LONG_W          NUMBER
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+SELECT DISTINCT CITY FROM STATION 
+WHERE (CITY NOT LIKE 'A%') AND (CITY NOT LIKE 'E%') AND (CITY NOT LIKE 'I%') AND (CITY NOT LIKE 'O%') AND (CITY NOT LIKE 'U%');

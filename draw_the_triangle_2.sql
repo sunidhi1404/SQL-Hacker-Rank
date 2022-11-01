f8341d3f332e65b2b07f60b602b4392155d7265c
Sunidhi Thakur <38562071+sunidhi1404@users.noreply.github.com>
2022-11-01 13:25:11 +0530

    Create draw_the_triangle_2.sql

---
+++ draw_the_triangle_2.sql
@@ -0,0 +1,25 @@
+/*
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+Write a query to print the pattern P(20).
+*/
+
+DELIMITER $$
+CREATE PROCEDURE draw_triangle(num INT)
+BEGIN
+    CREATE TEMPORARY TABLE final_result(i INT, star VARCHAR(255));
+    SET @i=0;
+    WHILE @i < num
+    DO
+        INSERT INTO final_result VALUES(@i, REPEAT('* ', @i + 1));
+        SET @i = @i + 1;
+    END WHILE;
+    SELECT star FROM final_result;
+END $$
+DELIMITER ;
+
+CALL draw_triangle(20);
